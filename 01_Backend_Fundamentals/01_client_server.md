5505c3ebed962f66a07be77a13e56c15703d09e1
MusiranaLohith <lohithkumar15p@gmail.com>
2026-08-11 22:58:21 +0530

    Added FastAPI learning and practice

---
+++ 01_Backend_Fundamentals/01_client_server.md
@@ -0,0 +1,32 @@
+# Client and Server
+
+## Client
+
+A client is a program or device that sends a request to a server.
+
+Examples:
+- Web browser
+- Mobile application
+- Frontend application
+- Another backend service
+
+## Server
+
+A server is a program that receives requests, processes them, and sends responses.
+
+## API
+
+An API is an interface that allows different software systems to communicate with each other.
+
+## HTTP
+
+HTTP is the protocol commonly used for communication between clients and web servers.
+
+## Basic Flow
+
+### Client-
+HTTP-Request-
+Server-
+Processing-
+HTTP Response-
+Client
---
+++ 02_FastAPI_Basics/01_first_app.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/hello")
+def home():
+    return{
+        "message" : "Hello.."
+    }
+
+@app.get("/about")
+def about():
+    return{
+        "message": "This is my FastAPI backend"
+    }
+
+@app.get("/users")
+def users():
+    return{
+        "users": ["Lohith", "Rahul", "Anu"]
+    }
+
+@app.get("/status")
+def status():
+    return{
+        "status": "running"
+    }
+
+# path params
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    return{
+        "user_id" : user_id
+    }
+
+@app.get("/products/{product_id}")
+def get_product_id(product_id):
+    return{
+        "product_id" : product_id
+    }
+
+@app.get("/students/{student_id}/courses/{course_id}")
+def get_details(student_id:int,course_id:int):
+    return{
+        "student_id" : student_id,
+        "course_id" : course_id
+    }
+
+@app.get("/students/{student_id}")
+def get_name_id(student_id:int):
+    if student_id == 1:
+        return{
+            "student_id" : student_id,
+            "name" : "Lohith"
+        }
+    elif student_id == 2:
+        return{
+            "student_id": student_id,
+            "name" : "Rahul"
+        }
+    elif student_id == 3:
+        return{
+            "student_id": student_id,
+            "name" : "Anu"
+        }
+    else:
+        return{
+            "message" : "Student Not FOund"
+        }
